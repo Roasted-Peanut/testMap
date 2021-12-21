1ee36ef40191107db4d87f3801ab521897f20b30
PhungPV <vanphungplus@gmail.com>
2021-12-21 12:05:17 +0700

    read file excel

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,49 @@
-import React, {useEffect} from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ImageBackground } from 'react-native';
 import XLSX from 'xlsx';
-import {writeFile, readFile} from 'react-native-fs';
-import RNFS from 'react-native-fs';
+import { writeFile, readFile } from 'react-native-fs';
+import FileAsset from 'react-native-file-asset';
 
 export default Home = () => {
-  readFile('../src/excel/130001_evacuation_area.csv', 'ascii').then(res => {
-    console.log(res);
-    const workbook = XLSX.read(res, {type: 'binary'});
-    /* DO SOMETHING WITH workbook HERE */
-  });
-
+  const [results, setResults] = useState([])
+  useEffect(() => {
+    getDataExcel()
+  })
+  const getDataExcel = () => {
+    //read file path android/src/main/assets
+    FileAsset.loadFilePath('130001_evacuation_area', 'xlsx')
+      .then((filePath) => {
+        readFile(filePath, 'ascii').then((res) => {
+          const workbook = XLSX.read(res, { type: "binary" });
+          //convert dATA workbook to array
+          var first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
+          //covert to Json
+          var data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+          const arrayKey = data[0];
+          const result = [];
+          data.forEach((item, index) => {
+            let object = {}
+            if (index !== 0) {
+              arrayKey.forEach((subItem, subIndex) => {
+                object = { ...object, [`col${subIndex}`]: { label: subItem, value: item[subIndex] } }
+              })
+              result.push(object)
+            }
+            return result,
+            setResults(result)
+          })
+        });
+      })
+      .catch((error) => {
+        console.log('load file path error', error);
+      });
+  }
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <ImageBackground
-        style={{width: '100%', height: '100%'}}
-        source={require('../src/asset/christmas-2021.png')}>
-        <Text style={{fontSize: 25, color: '#ff89'}}>Merry Christmas</Text>
+        style={{ width: '100%', height: '100%' }}
+        source={require('../src/assets/christmas-2021.png')}>
+        <Text style={{ fontSize: 25, color: '#ff89' }}>Merry Christmas</Text>
       </ImageBackground>
     </View>
   );
